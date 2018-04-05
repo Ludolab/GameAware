993f3c6d5ca81793b79f1c77946df2016b8c0061
raegu14 <raegu14@gmail.com>
2018-04-05 13:35:12 -0400

    need to edit typescript files to change gamelogic

--- Client/Typescript Src/ExampleGameFile.ts
+++ Client/Typescript Src/ExampleGameFile.ts
@@ -6,22 +6,22 @@ When the webpage (game.html) is opened, it will create a twitch video plugin and
 transparent HTML5 canvas on top of it, which is controlled by this code.
 
 If the corresponding Unity game is running, set up correctly, and streaming to Twitch,
-the webpage this client is embedded in will be recieving video of several fireflies flying
+the webpage this client is embedded in will be recieving video of several towers flying
 around in various curves while a drifting 3d camera watches them.
 
 This client will, meanwhile, be receiving metadata about the contents of the streaming
 video - specifically, it will be receiving screen space positional information about the
-various fireflies as well as real time game logic statistics about them.
+various towers as well as real time game logic statistics about them.
 
 What this specific file does is perform some client logic and graphics rendering with that
 metadata, over the video frame. Namely,
 
-1) if the user's mouse is over one of the fireflies, that firefly will be highlighted
+1) if the user's mouse is over one of the towers, that tower will be highlighted
 
-2) if the user clicks on a highlight firefly, the firefly will have a target put over it that will
-follow the firefly in realtime.
+2) if the user clicks on a highlight tower, the tower will have a target put over it that will
+follow the tower in realtime.
 
-3) If a firefly is highlighted, some statistics will be displayed about it.
+3) If a tower is highlighted, some statistics will be displayed about it.
 
 That's it!
 
@@ -48,13 +48,13 @@ function CacheGameAssets(c: Cacher): void {
 // These two interfaces are the parameters for network messages.  They'll be serialized into JSON and then trasmitted across the metadata server.
 // The specific fields need to stay in sync (by both type and name) with the C# code.
 
-interface ServerFirefly{
+interface Servertower{
     x: number;
 	y: number;
 	scale: number;
 }
-interface ServerFireflies{
-	items: ServerFirefly[];
+interface Servertowers{
+	items: Servertower[];
 }
 
 function InitializeGame(apg: APGSys): void {
@@ -64,11 +64,11 @@ function InitializeGame(apg: APGSys): void {
 	var phaserGameWorld: Phaser.Group = apg.w;
 
 	// This field will be the metadata sent down from the server for the current frame.  
-	var metadataForFrame: ServerFireflies = null;
+	var metadataForFrame: Servertowers = null;
 	// When the user clicks the mouse, we'll pause before they're allowed to click the mouse again.
 	var lastClickDelay: number = 0;
-	// Index in the ServerFireflies array of the currently selected firefly.  We'll default to showing the first firefly.
-	var fireflyID: number = 0;
+	// Index in the Servertowers array of the currently selected tower.  We'll default to showing the first tower.
+	var towerID: number = 0;
 
 	// Register the clicking sound
 	var clickSound: Phaser.Sound = apg.g.add.audio('assets/click.mp3', .4, false);
@@ -78,7 +78,7 @@ function InitializeGame(apg: APGSys): void {
 
 	// Setup callbacks with the metadata subsystem.
 	apg.ResetServerMessageRegistry();
-	apg.Register<ServerFireflies>("fireflies", updatedMetadataForNewFrame => {
+	apg.Register<Servertowers>("towers", updatedMetadataForNewFrame => {
 		// We register a simple callback that updates when the video frame has advanced and there is new metadata.
 		// We will use this metadata in game object frame updates to change what is displayed in the overlay.
 		// In theory, it would be more efficient to do the actual updating in this callback, but it's not a priority.
@@ -88,87 +88,87 @@ function InitializeGame(apg: APGSys): void {
 	// _____________________________________ MAKE GAME OBJECTS _______________________________________
 
 	// This app has four main game objects, three of which use updated metadata to change what
-	// graphics are drawn over the video frame.  There is a highlight that draws when a firefly is
-	// near the mouse cursor, there is a target that follows a firefly if it has been selected, there
-	// is a text label showing updating gameplay specific stats of the currently selected firefly, and
+	// graphics are drawn over the video frame.  There is a highlight that draws when a tower is
+	// near the mouse cursor, there is a target that follows a tower if it has been selected, there
+	// is a text label showing updating gameplay specific stats of the currently selected tower, and
 	// there is a background under that text that obscures that visually jarring binary frame data
 	// in the video stream.
 
 	// _____ Mouse Highlighter _______
 
-	// This is a highlight for the situation when the mouse cursor is roughly over one of the fireflies.
-	// It let's us tell the viewer that they could click on that firefly to target it.
+	// This is a highlight for the situation when the mouse cursor is roughly over one of the towers.
+	// It let's us tell the viewer that they could click on that tower to target it.
 	// We will also do the logic in this game object to see if the mouse is down, and if so, we will
-	// target that firefly, if one is highlighted, or untarget otherwise.
-	var fireflyMouseHighlight: Phaser.Sprite = new Phaser.Sprite(apg.g, 0, 0, 'assets/blueorb.png');
-	fireflyMouseHighlight.blendMode = PIXI.blendModes.ADD;
-	fireflyMouseHighlight.anchor = new Phaser.Point(.5, .5);
-	fireflyMouseHighlight.scale = new Phaser.Point(1, 1);
-	fireflyMouseHighlight.update = () => {
+	// target that tower, if one is highlighted, or untarget otherwise.
+	var towerMouseHighlight: Phaser.Sprite = new Phaser.Sprite(apg.g, 0, 0, 'assets/blueorb.png');
+	towerMouseHighlight.blendMode = PIXI.blendModes.ADD;
+	towerMouseHighlight.anchor = new Phaser.Point(.5, .5);
+	towerMouseHighlight.scale = new Phaser.Point(1, 1);
+	towerMouseHighlight.update = () => {
 		lastClickDelay--;
 		if (metadataForFrame != null) {
-			var overAFirefly:boolean = false;
-			var fireflyIndex = -1;
+			var overAtower:boolean = false;
+			var towerIndex = -1;
 			for (var k: number = 0; k < metadataForFrame.items.length; k++) {
 				// get the screen coordinates that have been passed down as metadata.
 				var x:number = APGHelper.ScreenX( metadataForFrame.items[k].x );
 				var y: number = APGHelper.ScreenY(metadataForFrame.items[k].y);
 
-				// Test if our mouse is close to the screen space coordinates of the current firefly.
+				// Test if our mouse is close to the screen space coordinates of the current tower.
 				// This test is simple and hard-coded for this demo.
 				if (Math.abs(apg.g.input.activePointer.x - x) < 48 && Math.abs(apg.g.input.activePointer.y - y) < 48) {
 
-					// We are over a firefly, so record its index.
-					fireflyIndex = k;
-					overAFirefly = true;
+					// We are over a tower, so record its index.
+					towerIndex = k;
+					overAtower = true;
 
-					// Center the highlight on this firefly and make it visible.
-					fireflyMouseHighlight.x = x;
-					fireflyMouseHighlight.y = y;
-					fireflyMouseHighlight.visible = true;
+					// Center the highlight on this tower and make it visible.
+					towerMouseHighlight.x = x;
+					towerMouseHighlight.y = y;
+					towerMouseHighlight.visible = true;
 				}
 			}
-			if (!overAFirefly) {
-				// The case where we are not over a firefly.  Make the highlight invisible and turn off targeting
+			if (!overAtower) {
+				// The case where we are not over a tower.  Make the highlight invisible and turn off targeting
 				// if the mouse was clicked.
-				fireflyMouseHighlight.visible = false;
+				towerMouseHighlight.visible = false;
 				if (apg.g.input.activePointer.isDown && lastClickDelay <= 0) {
-					fireflyID = -1;
+					towerID = -1;
 					lastClickDelay = 20;
 				}
 			}
 			else {
-				// The case where we are over a firefly.  If the mouse was clicked,
-				// play a sound and change the fireflyID.
+				// The case where we are over a tower.  If the mouse was clicked,
+				// play a sound and change the towerID.
 				if (apg.g.input.activePointer.isDown && lastClickDelay <= 0) {
-					fireflyID = fireflyIndex;
+					towerID = towerIndex;
 					clickSound.play();
 					lastClickDelay = 20;
 				}
 			}
 		}
 	}
-	phaserGameWorld.addChild(fireflyMouseHighlight);
+	phaserGameWorld.addChild(towerMouseHighlight);
 
-	// _____ Firefly Targeter _______
+	// _____ tower Targeter _______
 
-	// This is a target graphic that follows the currently selected firefly, if there is one.
-	var fireflyTargetGraphic: Phaser.Sprite = new Phaser.Sprite(apg.g, 0, 0, 'assets/hudselect.png');
-	fireflyTargetGraphic.blendMode = PIXI.blendModes.ADD;
-	fireflyTargetGraphic.anchor = new Phaser.Point(.5, .5);
-	fireflyTargetGraphic.scale = new Phaser.Point(1, 1);
-	fireflyTargetGraphic.update = () => {
+	// This is a target graphic that follows the currently selected tower, if there is one.
+	var towerTargetGraphic: Phaser.Sprite = new Phaser.Sprite(apg.g, 0, 0, 'assets/hudselect.png');
+	towerTargetGraphic.blendMode = PIXI.blendModes.ADD;
+	towerTargetGraphic.anchor = new Phaser.Point(.5, .5);
+	towerTargetGraphic.scale = new Phaser.Point(1, 1);
+	towerTargetGraphic.update = () => {
 
-		// if we are currently targeting a firefly, recenter the target graphic over the new screen space position of the
-		// selected firefly.
-		if ( fireflyID != -1 && metadataForFrame != null && metadataForFrame != undefined ) {
-			fireflyTargetGraphic.visible = true;
-			fireflyTargetGraphic.x = APGHelper.ScreenX(metadataForFrame.items[fireflyID].x);
-			fireflyTargetGraphic.y = APGHelper.ScreenY(metadataForFrame.items[fireflyID].y);
+		// if we are currently targeting a tower, recenter the target graphic over the new screen space position of the
+		// selected tower.
+		if ( towerID != -1 && metadataForFrame != null && metadataForFrame != undefined ) {
+			towerTargetGraphic.visible = true;
+			towerTargetGraphic.x = APGHelper.ScreenX(metadataForFrame.items[towerID].x);
+			towerTargetGraphic.y = APGHelper.ScreenY(metadataForFrame.items[towerID].y);
 		}
-		else fireflyTargetGraphic.visible = false;
+		else towerTargetGraphic.visible = false;
 	}
-	phaserGameWorld.addChild(fireflyTargetGraphic);
+	phaserGameWorld.addChild(towerTargetGraphic);
 
 	// _____ Background Graphic  _______
 
@@ -179,15 +179,15 @@ function InitializeGame(apg: APGSys): void {
 
 	// _____ Stats Text _______
 
-	// This is statistic text.  It will display game logic metadata for the currently selected firefly if, in fact, a firefly is currently selected.
-	var fireflyStatsText: Phaser.Text = new Phaser.Text(apg.g, 20, 10, "", { font: '16px Caveat Brush', fill: '#112' });
-	fireflyStatsText.update = () => {
-		if ( fireflyID != -1 && metadataForFrame != null && metadataForFrame != undefined) {
-			fireflyStatsText.visible = true;
-			fireflyStatsText.text = "ID: " + fireflyID + "\nScale " + Math.floor( metadataForFrame.items[fireflyID].scale / 10000 * 48 );
+	// This is statistic text.  It will display game logic metadata for the currently selected tower if, in fact, a tower is currently selected.
+	var towerStatsText: Phaser.Text = new Phaser.Text(apg.g, 20, 10, "", { font: '16px Caveat Brush', fill: '#112' });
+	towerStatsText.update = () => {
+		if ( towerID != -1 && metadataForFrame != null && metadataForFrame != undefined) {
+			towerStatsText.visible = true;
+			towerStatsText.text = "ID: " + towerID + "\nScale " + Math.floor( metadataForFrame.items[towerID].scale / 10000 * 48 );
 		}
-		else fireflyStatsText.visible = false;
+		else towerStatsText.visible = false;
 	}
-	phaserGameWorld.addChild(fireflyStatsText);
+	phaserGameWorld.addChild(towerStatsText);
 
 }
