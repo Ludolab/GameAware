05eee660122dd6d4ca984830618a77583f66dfa2
raegu14 <raegu14@gmail.com>
2018-05-01 16:39:34 -0400

    fixed towers

--- Client/website/GameLogic.js
+++ Client/website/GameLogic.js
@@ -13,12 +13,16 @@ function InitializeGame(apg) {
     var phaserGameWorld = apg.w;
     var lastClickDelay = 0;
     var clickSound = apg.g.add.audio('assets/click.mp3', .4, false);
+    var serverMetadataForFrame = null;
+    var metadataForFrame = null;
+    var enemyMetadataForFrame = null;
+    apg.ResetServerMessageRegistry();
+    apg.Register("server", function (updatedMetadataForNewFrame) {
+        serverMetadataForFrame = updatedMetadataForNewFrame;
+        metadataForFrame = serverMetadataForFrame.towerInfo;
+        enemyMetadataForFrame = serverMetadataForFrame.enemyInfo;
+    });
     {
-        var metadataForFrame = null;
-        apg.ResetServerMessageRegistry();
-        apg.Register("towers", function (updatedMetadataForNewFrame) {
-            metadataForFrame = updatedMetadataForNewFrame;
-        });
         var towerID = 0;
         var towerMouseHighlight = new Phaser.Sprite(apg.g, 0, 0, 'assets/TowerInformationPopup.png');
         towerMouseHighlight.anchor = new Phaser.Point(0.4, 0.75);
@@ -29,16 +33,20 @@ function InitializeGame(apg) {
                 var overAtower = false;
                 var towerIndex = -1;
                 for (var k = 0; k < metadataForFrame.items.length; k++) {
-                    var x = APGHelper.ScreenX(metadataForFrame.items[k].x);
-                    var y = APGHelper.ScreenY(metadataForFrame.items[k].y);
-                    var scaleX = APGHelper.ScreenX(metadataForFrame.items[k].scaleX);
-                    var scaleY = APGHelper.ScreenY(metadataForFrame.items[k].scaleY);
-                    if (apg.g.input.activePointer.x >= x && apg.g.input.activePointer.x <= x + scaleX &&
-                        apg.g.input.activePointer.y >= y && apg.g.input.activePointer.y <= y + scaleY) {
+                    var topX = APGHelper.ScreenX(metadataForFrame.items[k].x);
+                    var topY = APGHelper.ScreenY(metadataForFrame.items[k].y);
+                    var bottomX = APGHelper.ScreenX(metadataForFrame.items[k].scaleX + metadataForFrame.items[k].x);
+                    var bottomY = APGHelper.ScreenY(metadataForFrame.items[k].y - metadataForFrame.items[k].scaleY);
+                    if (apg.g.input.activePointer.y >= bottomY && apg.g.input.activePointer.y <= bottomY) {
+                        console.log("y is correct");
+                    }
+                    if (apg.g.input.activePointer.x >= topX && apg.g.input.activePointer.x <= bottomX &&
+                        apg.g.input.activePointer.y >= topY && apg.g.input.activePointer.y <= bottomY) {
+                        console.log("correct position");
                         towerIndex = k;
                         overAtower = true;
-                        towerMouseHighlight.x = x;
-                        towerMouseHighlight.y = y;
+                        towerMouseHighlight.x = topX;
+                        towerMouseHighlight.y = topY;
                         towerMouseHighlight.visible = true;
                         towerID = towerIndex;
                     }
@@ -55,30 +63,30 @@ function InitializeGame(apg) {
         phaserGameWorld.addChild(backgroundCoveringBinaryEncoding);
         var towerStatsText = new Phaser.Text(apg.g, towerMouseHighlight.x, towerMouseHighlight.y, "", { font: '12px Helvetica', fill: '#C0C0C0' });
         towerStatsText.anchor = new Phaser.Point(1.0, 1.35);
-        var towerStatsFireBar = new Phaser.Rectangle(0, 0, 0, 0);
+        var towerStatsFireBar = new Phaser.Graphics(apg.g, 0, 0);
         towerStatsText.update = function () {
             if (towerID != -1 && metadataForFrame != null && metadataForFrame != undefined) {
                 towerStatsText.x = towerMouseHighlight.x;
                 towerStatsText.y = towerMouseHighlight.y;
                 towerStatsText.visible = true;
                 towerStatsText.text = metadataForFrame.items[towerID].name + "\nFIRE RATE: \nATTACK:";
-                towerStatsFireBar.width = metadataForFrame.items[towerID].fireRate * 10;
+                towerStatsFireBar.width = metadataForFrame.items[towerID].fireRate * 100;
                 towerStatsFireBar.height = 10;
                 towerStatsFireBar.x = towerMouseHighlight.x;
                 towerStatsFireBar.y = towerMouseHighlight.y;
+                towerStatsFireBar.visible = true;
+                towerStatsFireBar.beginFill(0xff000);
+                towerStatsFireBar.drawRect(towerStatsFireBar.x, towerStatsFireBar.y, towerStatsFireBar.width, 100);
+                towerStatsFireBar.endFill();
             }
             else {
                 towerStatsText.visible = false;
+                towerStatsFireBar.kill();
             }
         };
         phaserGameWorld.addChild(towerStatsText);
     }
     {
-        var enemyMetadataForFrame = null;
-        apg.ResetServerMessageRegistry();
-        apg.Register("enemies", function (updatedMetadataForNewFrame) {
-            enemyMetadataForFrame = updatedMetadataForNewFrame;
-        });
         var enemyID = 0;
         var waveNumber = -1;
         var waveImages = new Array();
